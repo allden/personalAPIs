c441fc6b8ace5f8b868da082481464654207277d
allden <30358515+allden@users.noreply.github.com>
2020-06-22 11:17:44 +0300

    Added several other APIs as this is taking up a whole heroku server and I need to use those sparingly.

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ IP Lookup
 
 -------------------------------------------------------------------------
 
-An API that allows you to get user information based on IP address.
+A list of APIs that I use for my projects.
 
 It has two relevant routes:
 
-/ Which returns the data about the requester's IP address and
+"/iplookup" Which returns the data about the requester's IP address and
 
-/?ip=1.1.1.1 With 1.1.1.1 being placeholder data and returns information about the specified IP address.
+"/iplookup/?ip=1.1.1.1" With 1.1.1.1 being placeholder data and returns information about the specified IP address.
 
 The API url is: https://infinite-dusk-92659.herokuapp.com/
--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const geoip = require('geoip-lite');
+const fetch = require('node-fetch');
 
-router.get('/', (req, res) => {
+router.get('/iplookup', (req, res) => {
     let userIp = req.headers['x-forwarded-for'];
     if(userIp === null) {
         userIp = req.getRemoteAddr();
@@ -16,12 +17,64 @@ router.get('/', (req, res) => {
     };
     
     if (requestedIp && queryFilter.test(requestedIp) === false) {
-        return res.status(400).json({
-            message: 'Invalid IP address'
+        errorHandling({
+            res,
+            msg: 'Invalid IP address',
+            status: 400
         });
     };
 
     return res.json(geo);
 });
+
+// PLACES API FOURSQUARE
+router.get('/foursquare', (req, res) => {
+    const {ll, query, v, limit} = req.query;
+    const clientId = process.env.FOURSQUARE_CLIENT_ID;
+    const clientSecret = process.env.FOURSQUARE_CLIENT_SECRET;
+    let url = `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}&client_secret=${clientSecret}&ll=${ll}&query=${query}&v=${v}&limit=${limit}`;
+
+    fetch(url)
+    .then(fetchRes => fetchRes.json())
+    .then(data => res.json(data))
+    .catch(err => errorHandling({res, err}));
+});
+
+// ONE CALL WEATHER DATA
+router.get('/openweathermap', (req, res) => {
+    const {lat, lon, exclude, units} = req.query;
+    const appId = process.env.OPENWEATHERMAP_APPID;
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${appId}&units=${units}`;
+
+    fetch(url)
+    .then(fetchRes => fetchRes.json())
+    .then(data => res.json(data))
+    .catch(err => errorHandling({res, err}))
+});
+
+// FORWARD AND REVERSE GEOCODING
+router.get('/opencagedata', (req, res) => {
+    const {q} = req.query;
+    const key = process.env.OPENCAGEDATA_APPID;
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${q}&key=${key}`;
+
+    fetch(url)
+    .then(fetchRes => fetchRes.json())
+    .then(data => res.json(data))
+    .catch(err => errorHandling({res, err}));
+});
+
+// WHEN THINGS GO HORRIBLY WRONG
+function errorHandling(obj) {
+    let msg = obj.msg || 'Something went wrong.';
+    let status = obj.status || 500;
+    const {res, err} = obj;
+
+    if(err) console.error('ERROR:', err);
+
+    return res.status(status).json({
+        'message': msg
+    });
+};
 
 module.exports = router;
